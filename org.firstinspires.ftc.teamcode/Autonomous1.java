70b3c82015e4820d21b4d59c063b55b3b54edc12
Vaibhav <vaibhav.rangan@gmail.com>
2018-10-13 10:54:39 -0700

    just some updates

---
+++ org.firstinspires.ftc.teamcode/Autonomous1.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous
+
+public class Autonomous1 extends LinearOpMode {
+
+    @Override
+    public void runOpMode(){
+        RobotHW robot = new RobotHW();
+        AndroidSensors sensors = new AndroidSensors();
+        ElapsedTime runtime = new ElapsedTime();
+        RobotUpDown upDown = new RobotUpDown();
+
+        double maxPower = 0.8;
+
+        robot.init(hardwareMap);
+
+
+        waitForStart();
+
+        runtime.reset();
+
+        while(opModeIsActive()){
+
+            while(runtime.seconds() < 2.5){
+                upDown.robotDown();
+            }
+
+            while(runtime.seconds() < 3){
+                robot.leftBack.setPower(maxPower);
+                robot.leftFront.setPower(-maxPower);
+                robot.rightBack.setPower(-maxPower);
+                robot.rightFront.setPower(maxPower);
+            }
+
+            while(runtime.seconds() < 4){
+                upDown.robotUp();
+            }
+
+            while(runtime.seconds() < 6){
+                robot.leftBack.setPower(-maxPower);
+                robot.leftFront.setPower(-maxPower);
+                robot.rightBack.setPower(-maxPower);
+                robot.rightFront.setPower(-maxPower);
+            }
+            //*drive into depot
+
+
+        }
+
+    }
+}
