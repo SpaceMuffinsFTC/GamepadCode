05024113875a2c264a094dd61c132ee9918efa36
Vaibhav <vaibhav.rangan@gmail.com>
2018-11-16 19:13:11 -0800

    just some updates

--- org.firstinspires.ftc.teamcode/AutonomousDepot2.java
+++ org.firstinspires.ftc.teamcode/AutonomousDepot2.java
@@ -2,20 +2,14 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.DcMotorImpl;
-import com.qualcomm.robotcore.hardware.CRServoImpl;
-
-import org.firstinspires.ftc.teamcode.RobotHW;
 
 @Autonomous
 
 public class AutonomousDepot2 extends LinearOpMode{
 
     RobotHW robot = new RobotHW();
+    GoldDetector gold = new GoldDetector();
     ElapsedTime runtime = new ElapsedTime();
 
     public void runOpMode() throws InterruptedException{
@@ -30,6 +24,7 @@ public class AutonomousDepot2 extends LinearOpMode{
 
         runtime.reset();
 
+        //getting robot down
         while (runtime.seconds() <= 5.45){
             robot.linearSlide.setPower(-0.6);
         }
@@ -52,45 +47,72 @@ public class AutonomousDepot2 extends LinearOpMode{
         while (runtime.seconds() >= 7.75 && runtime.seconds() <= 12.25) {
             robot.linearSlide.setPower(0);
             turnRight(0.3);
-            telemetry.addData("Status: ", "Turned Right");
+            telemetry.addData("Status: ", "Turned out of hook");
             telemetry.update();
         }
 
+        //sampling
+        while(runtime.seconds()>= 12.25 && runtime.seconds()<=18.25){
+            if(gold.isFound()){
+                drive(0.5);
+                telemetry.addData("Sampled: ","Gold hit");
+                telemetry.update();
+            } else{
+                strafeLeft(0.4);
+                if(gold.isFound()){
+                    drive(0.5);
+                    telemetry.addData("Sampled: ","Gold hit");
+                    telemetry.update();
+                } else {
+                    strafeLeft(0.4);
+                    if(gold.isFound()){
+                        drive(0.5);
+                        telemetry.addData("Sampled: ","Gold hit");
+                    } else {
+                        telemetry.addData("Problem: ","We couldn't find a gold block");
+                        telemetry.update();
+                    }
+                }
+            }
+        }
+
         //driving towards depot
-        while(runtime.seconds()>=12.25 && runtime.seconds()<= 16){
+        while(runtime.seconds()>=18.25 && runtime.seconds()<= 21){
             drive(0.5);
+            telemetry.addData("Status: ","Driving towards depot" );
+            telemetry.update();
         }
         // turning so that the team marker faces the depot
-        while(runtime.seconds()>=16 && runtime.seconds()<=17.5){
+        while(runtime.seconds()>=21 && runtime.seconds()<=22.5){
             turnRight(0.5);
+            telemetry.addData("Status: ","Turned towards depot");
+            telemetry.update();
         }
         // moving backwards towards the depot to drop team marker
-        while(runtime.seconds()>=17.5 && runtime.seconds()<=19){
+        while(runtime.seconds()>=22.5 && runtime.seconds()<=24){
             drive(-0.4);
+            telemetry.addData("Status: ","Moving towards depot");
+            telemetry.update();
         }
 
         //dropping team marker
-        while(runtime.seconds()>=19 && runtime.seconds()<= 20.5){
+        while(runtime.seconds()>=24 && runtime.seconds()<= 25.5){
             robot.teamMarker.setPosition(0.2);
-        }
-
-        while (runtime.seconds() >= 20.5 && runtime.seconds()<= 22){
-            stopDriving();
+            telemetry.addData("Status: ","Dropping team marker");
+            telemetry.update();
         }
 
         //turns towards crater
-        while(runtime.seconds()>= 22 && runtime.seconds()<=23){
+        while(runtime.seconds()>= 25.5 && runtime.seconds()<=27){
             turnRight(0.4);
+            telemetry.addData("Status: ", "Turning towards crater");
+            telemetry.update();
         }
 
         //drives into crater
-        while(runtime.seconds()>=23 && runtime.seconds()<=27){
+        while(runtime.seconds()>=27 && runtime.seconds()<=230){
             drive(-0.5);
-
-        }
-
-        while(runtime.seconds()>=27 && runtime.seconds()<=30){
-            stopDriving();
+            telemetry.addData("Status: ","Driving into crater");
         }
 
 
@@ -122,5 +144,21 @@ public class AutonomousDepot2 extends LinearOpMode{
         robot.rightBack.setPower(-power);
         robot.leftFront.setPower(-power);
         robot.rightFront.setPower(-power);
+    }
+
+    //strafe left
+    public void strafeLeft(double power){
+        robot.leftBack.setPower(-power);
+        robot.rightBack.setPower(power);
+        robot.leftFront.setPower(power);
+        robot.rightFront.setPower(-power);
+    }
+
+    //strafe right
+    public void strafeRight(double power){
+        robot.leftBack.setPower(power);
+        robot.rightBack.setPower(-power);
+        robot.leftFront.setPower(-power);
+        robot.rightFront.setPower(power);
     }
 }
